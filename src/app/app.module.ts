09eee61e6b0d7732ac16a6dcf106f97987ee65a0
roicocoryAmigao <roicoroy@mercadoamigao.com>
2022-07-12 12:40:09 +0100

    install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,23 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgxStripeModule } from 'ngx-stripe';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    IonicStorageModule.forRoot(),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxStripeModule.forRoot(environment.STRIPE_KEY),
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,33 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+/* eslint-disable @typescript-eslint/naming-convention */
 
 export const environment = {
-  production: false
+  production: false,
+  API_BASE_PATH: 'http://localhost:1337/api',
+  BASE_PATH: 'http://localhost:1337',
+  MEDUSA_API_BASE_PATH: 'http://localhost:9000',
+  instrumentationKey: null,
+  STRIPE_KEY:'pk_test_2qqvb6DTqKondL46mnEjZ68e',
+  firebase: {
+    projectId: 'mercadoamigao-frontend',
+    appId: '1:780324673146:web:db705b2235d5556477cdc3',
+    storageBucket: 'mercadoamigao-frontend.appspot.com',
+    apiKey: 'AIzaSyCQJ0Ddh9C-L5ZBKFpFyjPeaLz_deyk614',
+    authDomain: 'mercadoamigao-frontend.firebaseapp.com',
+    messagingSenderId: '780324673146',
+    measurementId: 'G-PT2N07XM92',
+  },
+  populate: '?populate=*',
+  todo: '/todo',
+  products: '/products',
+  // Post:
+  medusa_store_login_customer: 'http://localhost:9000/store/auth/',
+  // Post:
+  medusa_store_register_customer: 'http://localhost:9000/store/customers',
+  // Get
+  medusa_store_products: 'http://localhost:9000/store/products',
+  medusa_regions: 'http://localhost:9000/store/regions/',
+  // Post
+  create_medusa_cart: 'http://localhost:9000/store/carts/',
+  // Get
+  medusa_store: 'http://localhost:9000/admin/store/',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
